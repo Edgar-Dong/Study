8c365c4efc34da67c4ae3e1a9cf8e39e4cd6badf
wuji.dhw <wuji.dhw@alibaba-inc.com>
2020-12-16 18:43:52 +0800

    navigation使用viewmodel进行数据共享

---
+++ app/src/main/java/com/android/example/jetpack/navigation/NavViewModelActivity.java
@@ -0,0 +1,16 @@
+package com.android.example.jetpack.navigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.example.R;
+
+public class NavViewModelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_view_model);
+    }
+}
---
+++ app/src/main/java/com/android/example/jetpack/navigation/OneFragment.java
@@ -0,0 +1,103 @@
+package com.android.example.jetpack.navigation;
+
+import android.os.Bundle;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+
+import com.android.example.R;
+import com.android.example.databinding.FragmentOneBinding;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link OneFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class OneFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public OneFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment OneFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static OneFragment newInstance(String param1, String param2) {
+        OneFragment fragment = new OneFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_one, container, false);
+        MyViewModel viewModel;
+        viewModel = ViewModelProviders.of(getActivity()).get(MyViewModel.class);
+        FragmentOneBinding binding;
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_one, container, false);
+        binding.setData(viewModel);
+        binding.setLifecycleOwner(getActivity());
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController controller = Navigation.findNavController(v);
+                controller.navigate(R.id.action_oneFragment_to_twoFragment);
+            }
+        });
+        binding.seekBar.setProgress(viewModel.getNumber().getValue());
+        binding.seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                viewModel.getNumber().setValue(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        return binding.getRoot();
+    }
+}
